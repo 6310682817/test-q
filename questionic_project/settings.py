8c92ad3efbebfde10f8a05d87b80acd01725ef68
6310682817 <70094208+6310682817@users.noreply.github.com>
2022-11-13 02:40:42 +0700

    Update settings.py

--- questionic_project/settings.py
+++ questionic_project/settings.py
@@ -136,7 +136,7 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['questionic.herokuapp.com',
+ALLOWED_HOSTS = ['test-questionic.herokuapp.com',
                  '127.0.0.1']
 
 #Cookie name. this can be whatever you want
